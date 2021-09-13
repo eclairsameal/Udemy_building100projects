557a618eed659f2ee006edde2729b1379fbdc870
Fenrir <fenrireclair@gmail.com>
2021-09-13 19:42:07 +0900

    Update Step2.py

--- day7-Beginner-Hangman/Step2.py
+++ day7-Beginner-Hangman/Step2.py
@@ -4,6 +4,7 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+#chosen_word = random.choice(word_list)
 #TODO-1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
