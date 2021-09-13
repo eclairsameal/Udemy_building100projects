b578ebe085a523530c3a340c7b2ca3439964f87e
Fenrir <fenrireclair@gmail.com>
2021-09-13 19:26:38 +0900

    完成step4

--- day7-Beginner-Hangman/Step4.py
+++ day7-Beginner-Hangman/Step4.py
@@ -1,16 +1,71 @@
 import random
 
-#Step 3 
+#Step 4 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+lives = 6
 #Create blanks
 display = ["_"for i in range(len(chosen_word))]
-print(display)
-#TODO-1: - Use a while loop to let the user guess again. 
-#The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). 
-#Then you can tell the user they've won.
+print(" ".join(display))
 end_of_game = False
 while not end_of_game:
     guess = input("guess a letter:").lower()
@@ -19,9 +74,19 @@ while not end_of_game:
     for i in range(len(chosen_word)):
         if chosen_word[i] == guess:
             display[i] = guess
-
-    print(display)
-    
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if not guess in chosen_word:
+        lives -= 1
+    if lives == 0:
+        end_of_game = True
+        print("You lose")
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(" ".join(display))
+    print(stages[lives])
+    #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win")
+    
