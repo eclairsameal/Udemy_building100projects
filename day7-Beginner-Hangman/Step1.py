cf0e2448aa139b34306a7e57b77afb9c6bf946e3
Fenrir <fenrireclair@gmail.com>
2021-09-13 19:59:46 +0900

    Update Step1.py

--- day7-Beginner-Hangman/Step1.py
+++ day7-Beginner-Hangman/Step1.py
@@ -3,11 +3,11 @@ import random
 #Step 1 
 
 word_list = ["aardvark", "baboon", "camel"]
-
+chosen_word = random.choice(word_list)
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 #chosen_word = random.choice(word_list)
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# print(f'Pssst, the solution is {chosen_word}.')
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("guess a letter:").lower()
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
