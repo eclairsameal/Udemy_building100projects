fe259e2502bc506f0cfa22aff0cc2325251b9294
Fenrir <fenrireclair@gmail.com>
2021-09-15 05:50:18 +0900

    完成位移字母的功能

--- day8-Beginner-Function Parameters and Caesar Cipher/caesar_cipher_1.py
+++ day8-Beginner-Function Parameters and Caesar Cipher/caesar_cipher_1.py
@@ -7,16 +7,25 @@ shift = int(input("Type the shift number:\n"))
 
 # TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-# e.g.
-#plain_text = "hello"
-#shift = 5
-#cipher_text = "mjqqt"
-# print output: "The encoded text is mjqqt"
+def encrypt(plain_text, shift_alphabet):
+    cipher_text = ""
+    for letter in plain_text:
+        new_position = alphabet.index(letter) + shift_alphabet
+        if new_position >= len(alphabet):
+            new_position -= len(alphabet)
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is: {cipher_text}")
+    # TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
+    # e.g.
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    # print output: "The encoded text is mjqqt"
 
 # HINT: How do you get the index of an item in a list:
 # https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
 # 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
+    # 'civilization' 中有 z 直接加 shift 的值會超出 list 的範圍
 # TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+encrypt(text, shift)
