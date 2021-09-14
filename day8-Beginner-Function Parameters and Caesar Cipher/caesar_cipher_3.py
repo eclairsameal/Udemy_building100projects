cc5e57c1a5676d6bb45b3fb3f598fc2d6186da4d
Fenrir <fenrireclair@gmail.com>
2021-09-15 06:31:30 +0900

    完成 caesar-cipher-3  優化程式碼

--- day8-Beginner-Function Parameters and Caesar Cipher/caesar_cipher_3.py
+++ day8-Beginner-Function Parameters and Caesar Cipher/caesar_cipher_3.py
@@ -6,26 +6,16 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
-
-def encrypt(plain_text, shift_alphabet):
-    cipher_text = ""
-    for letter in plain_text:
+def caesar(start_text, shift_alphabet, cipher_direction):
+    ans_text = ""
+    if cipher_direction == "decode":
+        shift_alphabet *= -1
+    for letter in start_text:
         new_position = alphabet.index(letter) + shift_alphabet
         if new_position >= len(alphabet):
             new_position -= len(alphabet)
-        cipher_text += alphabet[new_position]
-    print(f"The encoded text is: {cipher_text}")
+        ans_text += alphabet[new_position]
+    print(f"Here's the {direction}d result: {ans_text}")  
 
-def decrypt(cipher_text, shift_alphabet):
-    plain_text = ""
-    for letter in cipher_text:
-        new_position = alphabet.index(letter) - shift_alphabet
-        plain_text += alphabet[new_position]
-    print(f"The encoded text is {plain_text}")
-
-if direction == "encode":
-  encrypt(plain_text=text, shift_alphabet=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_alphabet=shift)
-  
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(start_text=text, shift_alphabet=shift, cipher_direction=direction)
