eda07ea1d6a8b78ebac9fdd3103b43fd86eac0c8
Fenrir <fenrireclair@gmail.com>
2021-09-16 05:07:20 +0900

    complete day-9-2-exercise

--- day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/day-9-2-exercise.py
+++ day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/day-9-2-exercise.py
@@ -14,11 +14,12 @@ travel_log = [
 
 #TODO: Write the function that will allow new countries
 #to be added to the travel_log. 👇
-
-
-
-
-
+def add_new_country(country, visits, cities):
+    travel_log.append({
+        "country":country, 
+        "visits":visits, 
+        "cities":cities
+    })
 #🚨 Do not change the code below
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
