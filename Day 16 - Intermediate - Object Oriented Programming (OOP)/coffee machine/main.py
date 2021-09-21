989e8e3659c3b73d061214beff9dda69f0a64fad
Fenrir <fenrireclair@gmail.com>
2021-09-22 06:36:04 +0900

    TODO

--- Day 16 - Intermediate - Object Oriented Programming (OOP)/coffee machine/main.py
+++ Day 16 - Intermediate - Object Oriented Programming (OOP)/coffee machine/main.py
@@ -1,3 +1,10 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# TODO 1 Print report
+# TODO 2 Check resources sufficient
+# TODO 3 Process coins
+# TODO 4 Check transaction(交易) successful
+# TODO 5 Make Coffee
+
