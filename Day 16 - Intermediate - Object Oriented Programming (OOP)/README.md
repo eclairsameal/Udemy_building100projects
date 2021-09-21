880b799909eb478917535515fdb9910b01e9e2a2
Fenrir <fenrireclair@gmail.com>
2021-09-22 07:01:13 +0900

    oop-coffee-machine-final

--- Day 16 - Intermediate - Object Oriented Programming (OOP)/README.md
+++ Day 16 - Intermediate - Object Oriented Programming (OOP)/README.md
@@ -28,3 +28,5 @@ A simple Python library for easily displaying tabular data in a visually appeali
 [oop-coffee-machine-start](https://replit.com/@appbrewery/oop-coffee-machine-start)
 
 [Coffee Machine Documentation](https://docs.google.com/document/d/e/2PACX-1vTragRHILyj76AvVgpWeOlEaLBXoxPM_43SdEyffIKtOgarj42SoSAsK6LwLAdHQs2qFLGthRZds6ok/pub)
+
+[oop-coffee-machine-final](https://replit.com/@appbrewery/oop-coffee-machine-final)
--- Day 16 - Intermediate - Object Oriented Programming (OOP)/coffee machine/main.py
+++ Day 16 - Intermediate - Object Oriented Programming (OOP)/coffee machine/main.py
@@ -8,3 +8,21 @@ from money_machine import MoneyMachine
 # TODO 4 Check transaction(交易) successful
 # TODO 5 Make Coffee
 
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+
+while is_on:
+    choice = input(f"What would you like? ({menu.get_items()}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
