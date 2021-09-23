f8146d6f4955ed5f285b8843453106cc3c180b42
Fenrir <fenrireclair@gmail.com>
2021-09-24 08:13:11 +0900

    將todo變成一般註解

--- Day 16 - Intermediate - Object Oriented Programming (OOP)/coffee machine/main.py
+++ Day 16 - Intermediate - Object Oriented Programming (OOP)/coffee machine/main.py
@@ -2,11 +2,11 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# TODO 1 Print report
-# TODO 2 Check resources sufficient
-# TODO 3 Process coins
-# TODO 4 Check transaction(交易) successful
-# TODO 5 Make Coffee
+#  1 Print report
+#  2 Check resources sufficient
+#  3 Process coins
+#  4 Check transaction(交易) successful
+#  5 Make Coffee
 
 
 menu = Menu()
