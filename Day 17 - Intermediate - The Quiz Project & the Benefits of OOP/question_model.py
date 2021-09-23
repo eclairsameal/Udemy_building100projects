1f5eff879cd73cacd73629152c0dc819ced96255
Fenrir <fenrireclair@gmail.com>
2021-09-24 08:56:36 +0900

    Create a Question class

---
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/question_model.py
@@ -0,0 +1,6 @@
+
+#   Create a Question class with an __init()__ method with tow attributes(text, answer)
+class Question():
+    def __init__(self, text, ans):
+        self.text = text
+        self.answer = ans
