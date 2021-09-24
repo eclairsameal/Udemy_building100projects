f254edab0812ce010df1e8d6757539e733905fcc
Fenrir <fenrireclair@gmail.com>
2021-09-24 10:31:17 +0900

    rewrite main.py

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main.py
@@ -1,15 +1,17 @@
 from question_model import Question
-from data import question_data
+#from data import question_data
 from quiz_brain import QuizBrain
+from data_op import question_data
 
 question_bank = []
-for q in question_data:
-    new_question = Question(q["text"], q["answer"])
+for q in question_data["results"]:
+    #new_question = Question(q["text"], q["answer"])
+    new_question = Question(q["question"], q["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-while quit.still_has_question():
-    quit.next_question()
+while quiz.still_has_question():
+    quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
