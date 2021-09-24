76a90e67c5ff10329fa40484ad429b46438e3a4b
Fenrir <fenrireclair@gmail.com>
2021-09-24 10:08:32 +0900

    紀錄score

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/quiz_brain.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/quiz_brain.py
@@ -1,6 +1,7 @@
 class QuizBrain():
     def __init__(self, q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def still_has_question(self):
@@ -20,9 +21,11 @@ class QuizBrain():
 
     def check_answer(self, user_ans, answer):
         if user_ans.lower() == answer.lower():
+            self.score += 1
             print("You got it right!")
         else:
             print("That's wrong.")
         print(f"The correct answer was: {answer}.")
-
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
 
