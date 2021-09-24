3487a453e9e43a98c35c6f85ce83bfb771dda7d8
Fenrir <fenrireclair@gmail.com>
2021-09-24 20:49:56 +0900

    updata

---
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/draw_line.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("DarkBlue")
+
+# draw a square
+for _ in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+screen = Screen()
+screen.exitonclick()
