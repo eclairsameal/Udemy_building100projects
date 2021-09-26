e202366d4b1f40e80b17a9fc4f12f041ae0ef7f4
Fenrir <fenrireclair@gmail.com>
2021-09-27 03:26:46 +0900

    Create and move a paddle in main.py

--- Day 22 - Intermediate - Build Pong -  The Famous Arcade Game/README.md
+++ Day 22 - Intermediate - Build Pong -  The Famous Arcade Game/README.md
@@ -17,3 +17,34 @@ Detect collision with paddle
 Detect when paddle misses
 
 Keep score
+
+## Create the screen
+
+width=800
+
+height=600
+
+## Create and move a paddle
+
+paddle:
+```
+    width = 20
+    height = 100
+    x_pos = 350
+    y_pos = 0
+    move = +-20
+```
+
+### animation
+```python
+import time
+screen.tracer(0)  # Close animation
+    .
+    .
+    .
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+```
+    
--- Day 22 - Intermediate - Build Pong -  The Famous Arcade Game/main.py
+++ Day 22 - Intermediate - Build Pong -  The Famous Arcade Game/main.py
@@ -1,9 +1,37 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)  # Close animation
+
+paddle = Turtle(shape="square")
+paddle.penup()
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(350, new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(350, new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
 
