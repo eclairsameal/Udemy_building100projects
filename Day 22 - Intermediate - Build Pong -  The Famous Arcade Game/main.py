97a25022d945776b83cf179b863ea1aba38b686c
Fenrir <fenrireclair@gmail.com>
2021-09-27 04:57:22 +0900

    Detect collision with wall and bounce

--- Day 22 - Intermediate - Build Pong -  The Famous Arcade Game/main.py
+++ Day 22 - Intermediate - Build Pong -  The Famous Arcade Game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
@@ -10,17 +11,22 @@ screen.tracer(0)  # Close animation
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+ball = Ball()
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    ball.move()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 screen.exitonclick()
 
