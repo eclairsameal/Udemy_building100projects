d8c6a8c5778ba790a94bde80faa5cf6cf39b1288
Fenrir <fenrireclair@gmail.com>
2021-09-29 04:57:31 +0900

    open file

--- Day 24 - Files, Directories and Paths/scoreboard.py
+++ Day 24 - Files, Directories and Paths/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0  # add
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -16,11 +17,14 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
+        self.clear()
         self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FORT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
 
