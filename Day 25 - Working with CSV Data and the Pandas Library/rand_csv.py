856aa849d77e92841e20e40a7aacc1b8716d097f
Fenrir <fenrireclair@gmail.com>
2021-09-30 06:49:25 +0900

    Get Data in Row

--- Day 25 - Working with CSV Data and the Pandas Library/rand_csv.py
+++ Day 25 - Working with CSV Data and the Pandas Library/rand_csv.py
@@ -16,20 +16,22 @@ data = pandas.read_csv("weather_data.csv")
 # print(type(data))
 # print(type(data["temp"]))
 
-data_dict = data.to_dict()
-print(data_dict)
-
-temp_list = data["temp"].to_list()
-print(temp_list)
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].to_list()
+# print(temp_list)
 # average = sum(temp_list)/len(temp_list)
 # print(average)
-print(data["temp"].mean()) # average
-print(data["temp"].max())
+# print(data["temp"].mean())  # average
+# print(data["temp"].max())
 
 # Get Data in Columns
-print(data["condition"])
-print(data.condition)
+# print(data["condition"])
+# print(data.condition)
 
 # Get Data in Row
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
 
 
