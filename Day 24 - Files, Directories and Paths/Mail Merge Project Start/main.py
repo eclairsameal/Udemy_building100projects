57e89aa09215cd7513fd58bd08151b13e995cf82
Fenrir <fenrireclair@gmail.com>
2021-09-30 05:33:39 +0900

    complete Mail Merge Project

--- Day 24 - Files, Directories and Paths/Mail Merge Project Start/main.py
+++ Day 24 - Files, Directories and Paths/Mail Merge Project Start/main.py
@@ -6,3 +6,18 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+letter = ""
+with open("Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+print(letter)
+
+with open("Input/Names/invited_names.txt") as name_file:
+    for name in name_file:
+        output_file_name = "Output/ReadyToSend/letter_for_" + name.strip() + ".txt"
+        with open(output_file_name, mode="w") as output_file:
+            letter_replace = letter.replace("[name]", name.strip())
+        #print(letter_replace)
+            output_file.write(letter_replace)
+
+
