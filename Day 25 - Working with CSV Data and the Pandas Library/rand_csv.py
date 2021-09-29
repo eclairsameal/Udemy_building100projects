7098d9378ba6cf7de7b4c72559438468a50ea78e
Fenrir <fenrireclair@gmail.com>
2021-09-30 06:27:38 +0900

    old method

--- Day 25 - Working with CSV Data and the Pandas Library/rand_csv.py
+++ Day 25 - Working with CSV Data and the Pandas Library/rand_csv.py
@@ -13,4 +13,13 @@
 
 import pandas
 data = pandas.read_csv("weather_data.csv")
-print(data)
+# print(type(data))
+# print(type(data["temp"]))
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+average = sum(temp_list)/len(temp_list)
+print(average)
