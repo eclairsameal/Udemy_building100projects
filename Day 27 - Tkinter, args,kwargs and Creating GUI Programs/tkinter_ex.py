e2e6d08d4d8835de931a91227fafe8df10d82026
Fenrir <fenrireclair@gmail.com>
2021-10-14 07:14:57 +0900

    button and entry

--- Day 27 - Tkinter, args,kwargs and Creating GUI Programs/tkinter_ex.py
+++ Day 27 - Tkinter, args,kwargs and Creating GUI Programs/tkinter_ex.py
@@ -10,7 +10,22 @@ my_label.pack()
 #my_label["text"] = "New Text"
 my_label.config(text="New Text")
 
+
+def button_clicked():
+    #my_label.config(text="Button got clicked")
+    my_label.config(text=input.get())
+    print("I got clicked")
+
+
 # Button
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry()
+input.pack()
+#print(input.get())
+
 
 
 window.mainloop()
