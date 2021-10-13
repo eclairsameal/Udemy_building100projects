7b96d9b23bb349f4482be670dc4c350c13b45411
Fenrir <fenrireclair@gmail.com>
2021-10-14 07:45:26 +0900

    物件外圍的距離(paddx, pady)

--- Day 27 - Tkinter, args,kwargs and Creating GUI Programs/tkinter_ex.py
+++ Day 27 - Tkinter, args,kwargs and Creating GUI Programs/tkinter_ex.py
@@ -10,6 +10,7 @@ def button_clicked():
 window = Tk()
 window.title("Tkinter Program")
 window.minsize(width=500, height=300)
+window.config(padx=50, pady=50)
 
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 #my_label.pack(side="left")
@@ -21,9 +22,12 @@ my_label.grid(column=0, row=0)
 button = Button(text="Click Me", command=button_clicked)
 button.grid(column=1, row=1)
 
+button2 = Button(text="New button")
+button2.grid(column=2, row=0)
+
 # Entry
 input = Entry()
-input.grid(column=2, row=2)
+input.grid(column=3, row=3)
 #print(input.get())
 
 
