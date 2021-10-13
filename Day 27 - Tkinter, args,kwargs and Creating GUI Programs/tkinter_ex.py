8f69a39a85e44bace9435bdc1284396b518fd04c
Fenrir <fenrireclair@gmail.com>
2021-10-14 07:39:37 +0900

    change to grid

--- Day 27 - Tkinter, args,kwargs and Creating GUI Programs/tkinter_ex.py
+++ Day 27 - Tkinter, args,kwargs and Creating GUI Programs/tkinter_ex.py
@@ -1,29 +1,29 @@
 #import tkinter
 from tkinter import *
 
-window = Tk()
-window.title("Tkinter Program")
-window.minsize(width=500, height=300)
-my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-#my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
-
 def button_clicked():
     #my_label.config(text="Button got clicked")
     my_label.config(text=input.get())
     print("I got clicked")
 
 
+window = Tk()
+window.title("Tkinter Program")
+window.minsize(width=500, height=300)
+
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+#my_label.pack(side="left")
+#my_label["text"] = "New Text"
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+
 # Button
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
 # Entry
 input = Entry()
-input.pack()
+input.grid(column=2, row=2)
 #print(input.get())
 
 
