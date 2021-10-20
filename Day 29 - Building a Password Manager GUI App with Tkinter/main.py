68209436ced6cb0d7daa82ad1576316e0e406706
Fenrir <fenrireclair@gmail.com>
2021-10-21 07:44:47 +0900

    寫入時會確認是否為空跟再次確認

--- Day 29 - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 29 - Building a Password Manager GUI App with Tkinter/main.py
@@ -1,18 +1,30 @@
 from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
 
-    with open("data.txt", "a") as file:
-        string = entry_website.get() + " | " + entry_email.get() + " | " + entry_password.get() + "\n"
-        # print(string)
-        file.write(string)
-        entry_website.focus()
-        entry_website.delete(0, END)  # 刪除文字(範圍), END = 字尾
-        entry_email.delete(0, END)
-        entry_email.insert(0, "@gmail.com")
-        entry_password.delete(0, END)
+def save():
+    """將資料寫入檔，寫入時會確認是否為空跟再次確認"""
+    website = entry_website.get()
+    email = entry_email.get()
+    password = entry_password.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Opps", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                      f"\nPassword: {password}\nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                string = website + " | " + email + " | " + password + "\n"
+                # print(string)
+                file.write(string)
+                entry_website.focus()
+                entry_website.delete(0, END)  # 刪除文字(範圍), END = 字尾
+                entry_email.delete(0, END)
+                entry_email.insert(0, "@gmail.com")
+                entry_password.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
