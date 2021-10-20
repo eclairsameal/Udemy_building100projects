b0284f021af688bdc576974c82ea19c668603035
Fenrir <fenrireclair@gmail.com>
2021-10-20 11:42:22 +0900

    set logo

--- Day 29 - Building a Password Manager GUI App with Tkinter/README.md
+++ Day 29 - Building a Password Manager GUI App with Tkinter/README.md
@@ -1,3 +1,5 @@
 # Day 29 - Building a Password Manager GUI App with Tkinter
 
 [color hunt](https://colorhunt.co/)
+
+[grid-columnspan-demo](https://replit.com/@appbrewery/grid-columnspan-demo#main.py)
--- Day 29 - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 29 - Building a Password Manager GUI App with Tkinter/main.py
@@ -1,5 +1,17 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+# Canvas
+canvas = Canvas(width=200, height=200)
+img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=img)
+canvas.pack()
+
+window.mainloop()
