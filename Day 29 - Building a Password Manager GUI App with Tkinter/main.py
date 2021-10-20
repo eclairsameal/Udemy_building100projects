3c20496b69862ce4c2335b5fc860a5b5e00aaa5a
Fenrir <fenrireclair@gmail.com>
2021-10-20 12:08:16 +0900

    ui

--- Day 29 - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 29 - Building a Password Manager GUI App with Tkinter/main.py
@@ -1,5 +1,4 @@
 from tkinter import *
-FONT_NAME = "Courier"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -13,24 +12,29 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=img)
-canvas.grid(row=0, column=2)
+canvas.grid(row=0, column=1)
 
 # Label
-label_website = Label(text="Website:", font=(FONT_NAME, 12))
+label_website = Label(text="Website:")
 label_website.grid(row=1, column=0)
-label_email = Label(text="Email/Username:", font=(FONT_NAME, 12))
+label_email = Label(text="Email/Username:")
 label_email.grid(row=2, column=0)
-label_password = Label(text="Password:", font=(FONT_NAME, 12))
+label_password = Label(text="Password:")
 label_password.grid(row=3, column=0)
 
 # Entry
-entry_website = Entry(width=35)
+entry_website = Entry(width=45)
 entry_website.grid(row=1, column=1, columnspan=2)
-entry_email = Entry(width=35)
+entry_email = Entry(width=45)
 entry_email.grid(row=2, column=1, columnspan=2)
-entry_password = Entry(width=21)
+entry_password = Entry(width=28)
 entry_password.grid(row=3, column=1)
 
+# Button
+button_password = Button(text="Generate Password", highlightthickness=0)
+button_password.grid(row=3, column=2)
+button_add = Button(text="Add",width=45)
+button_add.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
