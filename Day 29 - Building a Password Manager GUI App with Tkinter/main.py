31f74a632d8e4dda451567ddcf8bbd3f978c2f9f
Fenrir <fenrireclair@gmail.com>
2021-10-20 11:51:43 +0900

    set label

--- Day 29 - Building a Password Manager GUI App with Tkinter/main.py
+++ Day 29 - Building a Password Manager GUI App with Tkinter/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+FONT_NAME = "Courier"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -12,6 +13,15 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=img)
-canvas.pack()
+canvas.grid(row=0, column=2)
+
+# Label
+label_website = Label(text="Website:", font=(FONT_NAME, 12))
+label_website.grid(row=1, column=0)
+label_email = Label(text="Email/Username:", font=(FONT_NAME, 12))
+label_email.grid(row=2, column=0)
+label_password = Label(text="Password:", font=(FONT_NAME, 12))
+label_password.grid(row=3, column=0)
+
 
 window.mainloop()
