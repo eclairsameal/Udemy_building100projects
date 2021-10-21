1d883d9590c070fde4c4faff8aa403af13f4a700
Fenrir <fenrireclair@gmail.com>
2021-10-21 09:09:22 +0900

    complete exercise 2

--- Day 30 - Errors, Exceptions and JSON Data Improving the Password/day-30-2-exercise/ex.py
+++ Day 30 - Errors, Exceptions and JSON Data Improving the Password/day-30-2-exercise/ex.py
@@ -10,7 +10,11 @@ facebook_posts = [
 total_likes = 0
 
 for post in facebook_posts:
-    total_likes = total_likes + post['Likes']
+    try:
+        total_likes = total_likes + post['Likes']
+    except KeyError:
+        # total_likes += 0
+        pass
 
 
 print(total_likes)
