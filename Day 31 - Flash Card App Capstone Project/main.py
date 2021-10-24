56fcfa55642187a77ab525d052df9ad745b70e58
Fenrir <fenrireclair@gmail.com>
2021-10-24 11:49:53 +0900

    Create New Flash Cards

--- Day 31 - Flash Card App Capstone Project/main.py
+++ Day 31 - Flash Card App Capstone Project/main.py
@@ -1,6 +1,19 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+# print(data)
+to_learn = data.to_dict(orient="records")
+print(to_learn)
+
+
+def next_card():
+    current = random.choice(to_learn)
+    canvas.itemconfig(label_title, text="French")
+    canvas.itemconfig(label_word, text=current["French"])
 
 window = Tk()
 window.title("Flashy")
@@ -12,18 +25,19 @@ img_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=img_front)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 # Label in Canvas
-label_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-label_word = canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+label_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+label_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 
 # Button
 img_right = PhotoImage(file="images/right.png")
-button_right = Button(image=img_right, highlightthickness=0)
+button_right = Button(image=img_right, highlightthickness=0, command=next_card)
 button_right.grid(row=1, column=0)
 img_wrong = PhotoImage(file="images/wrong.png")
-button_wrong = Button(image=img_wrong, highlightthickness=0)
+button_wrong = Button(image=img_wrong, highlightthickness=0, command=next_card)
 button_wrong.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
