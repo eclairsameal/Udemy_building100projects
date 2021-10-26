e72c69ae5244e84496c965971e5307cd47d84ffc
Fenrir <fenrireclair@gmail.com>
2021-10-26 09:50:56 +0900

    complete Monday Motivation

--- Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Mondays_Email/main.py
+++ Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Mondays_Email/main.py
@@ -1,5 +1,26 @@
 import smtplib
+import datetime as dt
+import random
 
-g_email = "sisypheanephyra@gmail.com"
+g_email = ""
 g_password = ""
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
+
+if day_of_week == 1:
+    with open("quotes.txt", "r") as file:
+        data = file.readlines()
+    word = random.choice(data)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=g_email, password=g_password)
+        connection.sendmail(
+            from_addr=g_email,
+            to_addrs=g_email,
+            msg=f"Subject:Monday Motivation\n\n{word}")
+
+
+
+
 
