61ff536786d25cd208cede293df1f88c6d82a7ca
Fenrir <fenrireclair@gmail.com>
2021-11-09 05:26:49 +0900

    get iss_position

--- Day 33 - API Endpoints and API Parameters - ISS Overhead Notifier/ex.py
+++ Day 33 - API Endpoints and API Parameters - ISS Overhead Notifier/ex.py
@@ -1,4 +1,12 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+# print(response)
+response.raise_for_status()  # 根據回傳的結果引發 HTTP 錯誤
+data = response.json()
+# print(data)
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
