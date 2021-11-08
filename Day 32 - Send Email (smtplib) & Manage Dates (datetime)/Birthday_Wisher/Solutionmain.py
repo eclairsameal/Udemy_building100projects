a68e2c48e25351e5abb38dee41d9b2c783625fa0
Fenrir <fenrireclair@gmail.com>
2021-11-09 04:13:51 +0900

    編輯註解

--- Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Birthday_Wisher/Solutionmain.py
+++ Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Birthday_Wisher/Solutionmain.py
@@ -1,4 +1,4 @@
-#To run and test the code you need to update 4 places:
+# To run and test the code you need to update 4 places:
 # 1. Change MY_EMAIL/MY_PASSWORD to your own details.
 # 2. Go to your email provider and make it allow less secure apps.
 # 3. Update the SMTP ADDRESS to match your email provider.
