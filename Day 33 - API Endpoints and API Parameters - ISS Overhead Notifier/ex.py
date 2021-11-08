f71c10dfd80159b02f45b49dc11804c4ac88bdbf
Fenrir <fenrireclair@gmail.com>
2021-11-09 06:03:51 +0900

    利用Sunset and sunrise times API

--- Day 33 - API Endpoints and API Parameters - ISS Overhead Notifier/ex.py
+++ Day 33 - API Endpoints and API Parameters - ISS Overhead Notifier/ex.py
@@ -1,5 +1,9 @@
 import requests
 
+MY_LAT = 51.507351
+MY_LONG = -0.127758
+
+"""
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # print(response)
 response.raise_for_status()  # 根據回傳的結果引發 HTTP 錯誤
@@ -10,3 +14,16 @@ latitude = data["iss_position"]["latitude"]
 
 iss_position = (longitude, latitude)
 print(iss_position)
+"""
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data['results']['sunrise']
+sunset = data['results']['sunset']
+print(sunrise, sunset)
