030e4fe57b0cb02c6068284c4613a48f081d8711
Fenrir <fenrireclair@gmail.com>
2021-11-22 05:11:03 +0900

    Create a new Pixela user

---
+++ Day-37/main.py
@@ -0,0 +1,17 @@
+import requests
+
+
+""" 成功創立帳號後，就不用再執行
+pixela_endpoint = "https://pixe.la/v1/users"
+
+# post users 要求的參數dict
+user_params = {
+    "token": "",
+    "username": "",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+response = requests.post(pixela_endpoint, json=user_params) 
+print(response.text)
+"""
